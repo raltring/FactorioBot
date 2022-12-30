93657889a01291a7e18c17f55d8a36d2f9295ca9
ralt6 <ralt6@hotmail.com>
2022-12-29 21:04:34 -0600

    Added jupyter notebook for testing code. Added actual factorio images.

--- src/run.py
+++ src/run.py
@@ -2,10 +2,11 @@ import cv2
 import os
 
 if __name__ == '__main__':
-    #file = open('Test.txt','w')
-    #file.close()
-    #print(os.environ.get("PYTHONPATH"))
-    path = 'src/images/road.jpg'
-    image = cv2.imread(path)
-    h, w = image.shape[:2]
+    main_img_path = 'src/images/test_image_1.jpg'
+    enemy_img_path = 'src/images/ti1_dead_enemy_vertical.jpg'
+    main_img = cv2.imread(main_img_path)
+    enemy_img = cv2.imread(enemy_img_path)
+    h, w = main_img.shape[:2]
+    print("Height = {}, width = {}".format(h,w))
+    h,w = enemy_img.shape[:2]
     print("Height = {}, width = {}".format(h,w))
