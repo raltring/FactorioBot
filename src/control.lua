9d334330f63ff811a2ecf448f1bc00e2194a7320
ralt6 <ralt6@DESKTOP-N1832QO>
2023-01-07 22:04:26 -0600

    Added some actual functioning code

--- src/control.lua
+++ src/control.lua
@@ -1,3 +1,17 @@
-script.on_event(defines.events.on_tick)
-    --usefull commands: riding, walking, direction
-    game.player.walking_state = {walking = true, direction = defines.direction.north}
+--[[--]]
+local function update_direction(character, direction_in, is_walking)
+    character.walking_state = {walking = is_walking, direction = direction_in}
+end
+
+--[[--]]
+script.on_init(
+function()
+    game.print('Factorio Bot Loaded')
+end)
+
+--[[usefull commands: riding, walking, direction--]]
+script.on_event(defines.events.on_tick, 
+function()
+    local player = game.get_player(1)
+    update_direction(player,defines.direction.north,true)
+end)
